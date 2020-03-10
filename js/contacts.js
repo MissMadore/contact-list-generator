af6a09527df25aa1a6c48add6c3d09e406622ea0
Sabrina M <sd_madore@hotmail.com>
2020-03-10 10:05:10 -0400

    Add all required content
    Gosh damn I followed the heck out of this lesson

---
+++ js/contacts.js
@@ -0,0 +1,26 @@
+var peeps = [
+  {
+    name: 'Mercury Man',
+    email: 'theman@mercury.com',
+    tel: '+05551234',
+    loc: [46001200, 69816900]
+  },
+  {
+    name: 'Venus ’Venturer',
+    email: 'venus@theadventurer.com',
+    tel: '+15551234',
+    loc: [107477000, 108939000]
+  },
+  {
+    name: 'Jupiter Juggernaut',
+    email: 'thejugger@jupiter.com',
+    tel: '+55551234',
+    loc: [81604000, 740550000]
+  },
+  {
+    name: 'Buddy Stardust',
+    email: 'thebuuuuud@star.com',
+    tel: '+55555123',
+    loc: [12345678, 01234567]
+  }
+];
